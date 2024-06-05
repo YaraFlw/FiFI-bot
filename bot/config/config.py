d972b3652644380216314faa1265412ea263268a
YaraFlw <103410722+YaraFlw@users.noreply.github.com>
2024-06-05 08:36:12 +0300

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -4,19 +4,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
 
-    API_ID: int
-    API_HASH: str
+    API_ID: 22029450
+    API_HASH: sh: c02b324a5704a2e55872a519829d6ccd 
 
     MIN_AVAILABLE_ENERGY: int = 100
     SLEEP_BY_MIN_ENERGY: int = 200
 
     ADD_TAPS_ON_TURBO: int = 2500
 
-    AUTO_UPGRADE_TAP: bool = True
+    AUTO_UPGRADE_TAP: bool = False
     MAX_TAP_LEVEL: int = 5
-    AUTO_UPGRADE_ENERGY: bool = True
+    AUTO_UPGRADE_ENERGY: bool = False
     MAX_ENERGY_LEVEL: int = 5
-    AUTO_UPGRADE_CHARGE: bool = True
+    AUTO_UPGRADE_CHARGE: bool = False
     MAX_CHARGE_LEVEL: int = 5
 
     APPLY_DAILY_ENERGY: bool = True
